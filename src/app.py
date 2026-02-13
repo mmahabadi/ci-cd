dc227ab34f8cb564b80bdcfcbe5366bb863ecf9c
Mohsen <m.mehabadi@gmail.com>
2026-02-14 00:26:22 +0100

    add ci github workflow

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ def details():
     return jsonify({
         "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "hostname": socket.gethostname(),
-        "ip": socket.gethostbyname(socket.gethostname())
+        "ip": socket.gethostbyname(socket.gethostname()),
+        "status": "up"
     })
 
 @app.route("/api/v1/healthz")
