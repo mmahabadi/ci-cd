52c54eea63af6c3fcd7dcf8262ef8937a2760ce1
Mohsen <m.mehabadi@gmail.com>
2026-02-14 15:21:18 +0100

    Modifies values file on CD step

--- src/app.py
+++ src/app.py
@@ -10,7 +10,8 @@ def details():
         "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "hostname": socket.gethostname(),
         "ip": socket.gethostbyname(socket.gethostname()),
-        "status": "up :)"
+        "status": "up",
+        "message": "You are doing great! :)"
     })
 
 @app.route("/api/v1/healthz")
