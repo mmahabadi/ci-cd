823af004a8039082ef43060a39cfcb6ee5d60b1f
Mohsen <m.mehabadi@gmail.com>
2026-02-14 02:00:08 +0100

    Adds argocd installation

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
         "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "hostname": socket.gethostname(),
         "ip": socket.gethostbyname(socket.gethostname()),
-        "status": "up :)"
+        "status": "up"
     })
 
 @app.route("/api/v1/healthz")
